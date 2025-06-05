ca55b40f6a0f04f48f314667d09026266a925452
Jessica Mlangeni <jessica.mlangeni@gmail.com>
2025-06-05 07:53:47 +0200

    removed navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import ProductGrid from './components/ProductGrid';
 import BagSidebar from './components/BagSidebar';
-import Navbar from './components/Navbar';
-import ProductCard from './components/ProductCard';
+/*import Navbar from './components/Navbar';
+import ProductCard from './components/ProductCard';*/
 
 function App() {
   return (
